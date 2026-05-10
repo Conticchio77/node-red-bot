8fed9a00e3d775985ef127a330cea17894564c0d
Conticchio77 <marcod111077@gmail.com>
2026-05-11 00:41:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
     uiHost: "0.0.0.0",
 
     // Directory dati runtime (volume persistente su Railway o /app/data)
-    userDir: "/app/data",
+    userDir: "/data",
 
     // File del flow da caricare all'avvio
     flowFile: "flows.json",
